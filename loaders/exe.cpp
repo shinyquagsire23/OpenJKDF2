6d79b465a485cfdcd07568cea815c03690c539de
shinyquagsire23 <mtinc2@gmail.com>
2018-07-19 11:58:31 -0600

    loaders: I forgot this

---
+++ loaders/exe.cpp
@@ -0,0 +1,161 @@
+#include "exe.h"
+
+#include "main.h"
+#include "vm.h"
+
+uint32_t load_executable(uint32_t *image_addr, void **image_mem, uint32_t *image_size, uint32_t *stack_addr, uint32_t *stack_size)
+{
+    struct DosHeader dosHeader;
+    struct COFFHeader coffHeader;
+    struct PEOptHeader peHeader;
+    
+    FILE *f = fopen("JK.EXE", "rb");
+    if (!f)
+    {
+        printf("Failed to open JK.EXE, exiting\n");
+        return -1;
+    }
+    
+    fread(&dosHeader, sizeof(struct DosHeader), 1, f);
+    fseek(f, dosHeader.e_lfanew + sizeof(uint32_t), SEEK_SET);
+    fread(&coffHeader, sizeof(struct COFFHeader), 1, f);
+    
+    printf("PE header at 0x%08x\n", dosHeader.e_lfanew);
+    printf("COFF:\nMachine %x\nNum Sections %x\nTimeDateStamp %x\nSymbol Table Ptr %x\nNum Symbols %x\nOpt Hdr Size %x\nCharacteristics %x\n", coffHeader.machine, coffHeader.numberOfSections, coffHeader.timeDateStamp, coffHeader.pointerToSymbolTable, coffHeader.numberOfSymbols, coffHeader.sizeOfOptionalHeader, coffHeader.characteristics);
+    
+    if (coffHeader.machine != COFF_I386)
+    {
+        printf("Bad COFF machine type %x, expected %x, exiting\n", coffHeader.machine, COFF_I386);
+        return -1;
+    }
+    
+    fread(&peHeader, sizeof(struct PEOptHeader), 1, f);
+    
+    printf("Code size %x section starts at %x, execution starts at %x, %x\n", peHeader.sizeOfCode, peHeader.baseOfCode + peHeader.imageBase, peHeader.addressOfEntryPoint, peHeader.sizeOfImage);
+    
+    //TODO: should this be here
+    printf("Stack size %x, %x heap %x %x\n", peHeader.sizeOfStackReserve, peHeader.sizeOfStackCommit, peHeader.sizeOfHeapReserve, peHeader.sizeOfHeapCommit);
+    
+    *stack_addr = peHeader.imageBase + peHeader.sizeOfImage;
+    *stack_size = peHeader.sizeOfStackReserve;
+    
+    *image_addr = peHeader.imageBase;
+    //*image_mem = malloc(peHeader.sizeOfImage + peHeader.sizeOfStackReserve);
+    *image_mem = vm_alloc(peHeader.sizeOfImage + peHeader.sizeOfStackReserve);
+    *image_size = peHeader.sizeOfImage;
+    
+    for (int i = 0; i < coffHeader.numberOfSections; i++)
+    {
+        uint64_t temp;
+        struct PESection peSection;
+        fread(&peSection, sizeof(struct PESection), 1, f);
+        temp = ftell(f);
+        
+        printf("Section %.8s size 0x%x, vsize 0x%x, vaddr 0x%0x at file 0x%x. %x relocs, %x lines\n", peSection.name, peSection.sizeOfRawData, peSection.addr.virtualSize, peSection.virtualAddress, peSection.pointerToRawData, peSection.numberOfRelocations, peSection.numberOfLinenumbers);
+        
+        fseek(f, peSection.pointerToRawData, SEEK_SET);
+        fread(*image_mem + peSection.virtualAddress, peSection.sizeOfRawData, 1, f);
+        
+        fseek(f, temp, SEEK_SET);
+    }
+    
+    // Iterate directories and link
+    for (int i = 0; i < 16; i++)
+    {
+        //printf("directory %i, %x size %x\n", i, peHeader.dataDirectory[i].virtualAddress, peHeader.dataDirectory[i].size);
+        
+        struct ImportDesc tmp;
+        if (i != IMAGE_DIRECTORY_ENTRY_IMPORT) continue;
+
+        for (int j = 0; j < peHeader.dataDirectory[i].size / sizeof(struct ImportDesc); j++)
+        {
+            memcpy(&tmp, *image_mem + peHeader.dataDirectory[i].virtualAddress + j*sizeof(struct ImportDesc), sizeof(struct ImportDesc));
+
+            std::string name = std::string((char*)(*image_mem + tmp.name));
+            printf("%s:\n", name.c_str());
+            
+            printf("%x %x\n", peHeader.imageBase + tmp.name_desc_ptr, peHeader.imageBase + tmp.import_ptr_list);
+            
+            for (int i = 0; true; i++)
+            {
+                uint32_t importEntryRelAddr = *(uint32_t*)(*image_mem + tmp.name_desc_ptr + i*sizeof(uint32_t));
+                
+                if (!importEntryRelAddr) break;
+                if (importEntryRelAddr & 0x80000000)
+                {
+                    uint32_t index = importEntryRelAddr & ~0x80000000;
+                    std::string to_register = "";
+                    if (!strcmp(name.c_str(), "COMCTL32.dll") && index == 17)
+                    {
+                        to_register = "InitCommonControls";
+                    }
+                    else if (!strcmp(name.c_str(), "DPLAYX.dll") && index == 4)
+                    {
+                        to_register = "DirectPlayLobbyCreateA";
+                    }
+                    else if (!strcmp(name.c_str(), "smackw32.DLL"))
+                    {
+                        bool skip = false;
+                        switch (index)
+                        {
+                            case 38:
+                                to_register = "SmackSoundUseDirectSound";
+                                break;
+                            case 21:
+                                to_register = "SmackNextFrame";
+                                break;
+                            case 18:
+                                to_register = "SmackClose";
+                                break;
+                            case 14:
+                                to_register = "SmackOpen";
+                                break;
+                            case 26:
+                                to_register = "SmackGetTrackData";
+                                break;
+                            case 19:
+                                to_register = "SmackDoFrame";
+                                break;
+                            case 23:
+                                to_register = "SmackToBuffer";
+                                break;
+                            case 32:
+                                to_register = "SmackWait";
+                                break;
+                            case 17:
+                                to_register = "SmackSoundOnOff";
+                                break;
+                            default:
+                                printf("Unknown index %i for %s\n", index, name.c_str());
+                                skip = true;
+                                break;
+                        }
+                        
+                        if (skip) continue;
+                    }
+                    else
+                    {
+                        printf("Unknown index %i for %s\n", index, name.c_str());
+                        continue;
+                    }
+                    
+                    register_import(name, to_register, peHeader.imageBase + tmp.import_ptr_list + i*sizeof(uint32_t));
+                    printf("%s:%s at 0x%x\n", name.c_str(), to_register.c_str(), peHeader.imageBase + tmp.import_ptr_list + i*sizeof(uint32_t));
+                    continue;
+                }
+                
+                uint16_t hint = *(uint16_t*)(*image_mem + importEntryRelAddr);
+
+                std::string funcName = std::string((char*)(*image_mem + importEntryRelAddr + sizeof(uint16_t)));
+                register_import(name, funcName, peHeader.imageBase + tmp.import_ptr_list + i*sizeof(uint32_t));
+
+                printf("%s:%s at 0x%x\n", name.c_str(), funcName.c_str(), peHeader.imageBase + tmp.import_ptr_list + i*sizeof(uint32_t));
+            }
+            printf("\n");
+        }
+    }
+    
+    fclose(f);
+
+    return peHeader.imageBase + peHeader.addressOfEntryPoint;
+}
---
+++ loaders/exe.h
@@ -0,0 +1,132 @@
+#ifndef EXE_H
+#define EXE_H
+
+#include <unicorn/unicorn.h>
+
+
+struct DosHeader
+{
+     uint8_t signature[2];
+     uint16_t lastsize;
+     uint16_t nblocks;
+     uint16_t nreloc;
+     uint16_t hdrsize;
+     uint16_t minalloc;
+     uint16_t maxalloc;
+     uint16_t ss; // 2 byte ptr
+     uint16_t sp; // 2 byte ptr
+     uint16_t checksum;
+     uint16_t ip; // 2 byte ptr
+     uint16_t cs; // 2 byte ptr
+     uint16_t relocpos;
+     uint16_t noverlay;
+     uint16_t reserved1[4];
+     uint16_t oem_id;
+     uint16_t oem_info;
+     uint16_t reserved2[10];
+     uint32_t  e_lfanew;
+};
+
+struct COFFHeader
+{
+    uint16_t machine;
+    uint16_t numberOfSections;
+    uint32_t timeDateStamp;
+    uint32_t pointerToSymbolTable;
+    uint32_t numberOfSymbols;
+    uint16_t sizeOfOptionalHeader;
+    uint16_t characteristics;
+};
+
+#define COFF_I386 0x14c
+
+struct data_directory
+{ 
+    uint32_t virtualAddress;
+    uint32_t size;
+};
+
+struct PEOptHeader
+ {
+    uint16_t signature; //decimal number 267 for 32 bit, 523 for 64 bit, and 263 for a ROM image. 
+    uint8_t majorLinkerVersion; 
+    uint8_t minorLinkerVersion;
+    uint32_t sizeOfCode;
+    uint32_t sizeOfInitializedData;
+    uint32_t sizeOfUninitializedData;
+    uint32_t addressOfEntryPoint;  //The RVA of the code entry point
+    uint32_t baseOfCode;
+    uint32_t baseOfData;
+    /*The next 21 fields are an extension to the COFF optional header format*/
+    uint32_t imageBase;
+    uint32_t sectionAlignment;
+    uint32_t fileAlignment;
+    uint16_t majorOSVersion;
+    uint16_t minorOSVersion;
+    uint16_t majorImageVersion;
+    uint16_t minorImageVersion;
+    uint16_t majorSubsystemVersion;
+    uint16_t minorSubsystemVersion;
+    uint32_t win32VersionValue;
+    uint32_t sizeOfImage;
+    uint32_t sizeOfHeaders;
+    uint32_t checksum;
+    uint16_t subsystem;
+    uint16_t DLLCharacteristics;
+    uint32_t sizeOfStackReserve;
+    uint32_t sizeOfStackCommit;
+    uint32_t sizeOfHeapReserve;
+    uint32_t sizeOfHeapCommit;
+    uint32_t loaderFlags;
+    uint32_t numberOfRvaAndSizes;
+    struct data_directory dataDirectory[16];
+};
+
+struct PESection
+{
+    char name[8];
+    union {
+        uint32_t physicalAddress;
+        uint32_t virtualSize;
+    } addr;
+    uint32_t virtualAddress;
+    uint32_t sizeOfRawData;
+    uint32_t pointerToRawData;
+    uint32_t pointerToRelocations;
+    uint32_t pointerToLinenumbers;
+    uint16_t numberOfRelocations;
+    uint16_t numberOfLinenumbers;
+    uint32_t characteristics;
+};
+
+#pragma pack(push, 1)
+
+struct ImportDesc {
+	uint32_t name_desc_ptr;
+	uint32_t timestamp;
+	uint32_t forwarder_chain;
+	uint32_t name;
+	uint32_t import_ptr_list;
+};
+
+#pragma pack(pop)
+
+#define IMAGE_DIRECTORY_ENTRY_EXPORT	        0
+#define IMAGE_DIRECTORY_ENTRY_IMPORT	        1
+#define IMAGE_DIRECTORY_ENTRY_RESOURCE	        2
+#define IMAGE_DIRECTORY_ENTRY_EXCEPTION	        3
+#define IMAGE_DIRECTORY_ENTRY_SECURITY	        4
+#define IMAGE_DIRECTORY_ENTRY_BASERELOC	        5
+#define IMAGE_DIRECTORY_ENTRY_DEBUG	            6
+#define IMAGE_DIRECTORY_ENTRY_ARCHITECTURE	    7
+#define IMAGE_DIRECTORY_ENTRY_GLOBALPTR	        8
+#define IMAGE_DIRECTORY_ENTRY_TLS	            9
+#define IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG	    10
+#define IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT	    11
+#define IMAGE_DIRECTORY_ENTRY_IAT	            12
+#define IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT	    13
+#define IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR	14
+
+uint32_t load_executable(uint32_t *image_addr, void **image_mem, uint32_t *image_size, uint32_t *stack_addr, uint32_t *stack_size);
+
+#endif // EXE_H
