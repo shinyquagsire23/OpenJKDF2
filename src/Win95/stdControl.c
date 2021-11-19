4ee0c29c9ede21c43a447a2367fd9f8757313583
shinyquagsire23 <mtinc2@gmail.com>
2021-11-18 21:06:22 -0800

    Hacky tempfix around some weird MacOS mouse jerking

--- src/Win95/stdControl.c
+++ src/Win95/stdControl.c
@@ -88,6 +88,7 @@ float stdControl_GetAxis2(int a)
             axisAmt += -1.0;
         }
         
+        Window_lastSampleMs = 6;
         axisAmt += (float)Window_lastXRel * -((double)Window_lastSampleMs / 44.0);
         
         Window_lastXRel = 0;
@@ -118,6 +119,7 @@ float stdControl_GetAxis2(int a)
             axisAmt += -1.0;
         }
         
+        Window_lastSampleMs = 6;
         axisAmt += (float)Window_lastYRel * -((double)Window_lastSampleMs / 44.0);
         
         Window_lastYRel = 0;
