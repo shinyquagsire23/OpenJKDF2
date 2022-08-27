4c7c1605324881523381e3a9e188f17dd66c7cc0
shinyquagsire23 <mtinc2@gmail.com>
2022-08-26 21:31:38 -0700

    try Linux build

---
+++ build_linux64.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+mkdir -p build_linux64 && cd build_linux64
+
+mkdir -p build_protoc && cd build_protoc
+cmake -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF ../../3rdparty/protobuf/cmake
+make -j10 protoc
+cd ..
+
+mkdir -p build_protobuf && cd build_protobuf
+cmake -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_SHARED_LIBS=ON ../../3rdparty/protobuf/cmake
+make -j10 install
+cd ..
+
+mkdir -p build_gns && cd build_gns
+GNS_BUILD=$(pwd)
+cmake -DCMAKE_BUILD_TYPE=Release -DProtobuf_USE_STATIC_LIBS=ON -DProtobuf_LIBRARIES="-L$GNS_BUILD/../build_protobuf/lib" -DProtobuf_LIBRARIES_PATH="$GNS_BUILD/../build_protobuf/lib" -DProtobuf_INCLUDE_DIR=$GNS_BUILD/../../3rdparty/protobuf/src -DProtobuf_INCLUDE_DIR2=$GNS_BUILD/../build_protobuf/include -DProtobuf_PROTOC_EXECUTABLE=$GNS_BUILD/../build_protoc/protoc $GNS_BUILD/../../3rdparty/GameNetworkingSockets
+make -j10
+cd ..
+
+cmake .. &&
+make -j10 &&
+cd ..
