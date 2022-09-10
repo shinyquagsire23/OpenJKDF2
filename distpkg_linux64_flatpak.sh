fac60322dd5346e2c926ee04501dd114617e6b2b
shinyquagsire23 <mtinc2@gmail.com>
2022-09-10 15:00:40 -0700

    Fix

--- distpkg_linux64_flatpak.sh
+++ distpkg_linux64_flatpak.sh
@@ -5,12 +5,12 @@ rm -rf build_linux64
 
 commit=$(git log -n 1 --pretty=format:"%H")
 
-sed 's/REPLACE_COMMIT_HASH/$commit/g' packaging/flatpak/org.openjkdf2.OpenJKDF2.template.yml > org.openjkdf2.OpenJKDF2.yml
+sed "s/REPLACE_COMMIT_HASH/$commit/g" packaging/flatpak/org.openjkdf2.OpenJKDF2.template.yml > org.openjkdf2.OpenJKDF2.yml
 
 #bash packaging/flatpak/generate_icons.sh packaging/icon.png packaging/flatpak/icons
 
-flatpak-builder --user --repo=flatpak_openjkdf2 --arch=aarch64 build_linux_arm64_flatpak org.openjkdf2.OpenJKDF2.yml
-#flatpak-builder --user --repo=flatpak_openjkdf2 --arch=x86_64 build_linux_x86_64_flatpak org.openjkdf2.OpenJKDF2.yml
+flatpak-builder --user --repo=flatpak_openjkdf2 --force-clean --arch=aarch64 build_linux_arm64_flatpak org.openjkdf2.OpenJKDF2.yml
+#flatpak-builder --user --repo=flatpak_openjkdf2 --force-clean --arch=x86_64 build_linux_x86_64_flatpak org.openjkdf2.OpenJKDF2.yml
 
 flatpak build-bundle flatpak_openjkdf2 OpenJKDF2.flatpak org.openjkdf2.OpenJKDF2
 
