127b7706732f8fc18333fb6c750905edc054caf2
shinyquagsire23 <mtinc2@gmail.com>
2022-09-10 11:11:21 -0700

    Flatpak fixups

--- distpkg_linux64_flatpak.sh
+++ distpkg_linux64_flatpak.sh
@@ -1,10 +1,13 @@
 #!/bin/zsh
-rm -rf build_linux64_flatpak
+rm -rf build_linux_arm64_flatpak
+rm -rf build_linux_x86_64_flatpak
 rm -rf build_linux64
 
 bash packaging/flatpak/generate_icons.sh packaging/icon.png packaging/flatpak/icons
 
-flatpak-builder --user --install --force-clean build_linux64_flatpak org.openjkdf2.OpenJKDF2.yml
-flatpak install openjkdf2-origin org.openjkdf2.OpenJKDF2.Debug
+flatpak-builder --user --repo=openjkdf2 --force-clean --arch=aarch64 build_linux_arm64_flatpak org.openjkdf2.OpenJKDF2.yml
+flatpak-builder --user --repo=openjkdf2 --force-clean --arch=x86_64 build_linux_x86_64_flatpak org.openjkdf2.OpenJKDF2.yml
+flatpak install openjkdf2 org.openjkdf2.OpenJKDF2
+flatpak install openjkdf2 org.openjkdf2.OpenJKDF2.Debug
 #flatpak run org.openjkdf2.OpenJKDF2
 #flatpak run --command=sh --devel org.openjkdf2.OpenJKDF2
--- packaging/flatpak/build_linux64_flatpak.sh
+++ packaging/flatpak/build_linux64_flatpak.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+# Install build dependencies
+pip3 install cogapp
+
 mkdir -p build_linux64 && cd build_linux64
 
 if [ ! -f build_protoc/protoc ]; then
     mkdir -p build_protoc && cd build_protoc
     cmake -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF ../../3rdparty/protobuf/cmake
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     make -j10 protoc
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     cd ..
 fi
 
 if [ ! -f libprotobuf.so.3.21.4.0 ]; then
     mkdir -p build_protobuf && cd build_protobuf
     cmake -DCMAKE_INSTALL_PREFIX=. -DCMAKE_BUILD_TYPE=Release -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_SHARED_LIBS=ON ../../3rdparty/protobuf/cmake
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     make -j10 install
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     cd ..
     cp build_protobuf/libprotobuf.so.3.21.4.0 .
 fi
@@ -21,7 +28,9 @@ if [ ! -f libGameNetworkingSockets.so ]; then
     mkdir -p build_gns && cd build_gns
     GNS_BUILD=$(pwd)
     cmake -DCMAKE_BUILD_TYPE=Release -DProtobuf_USE_STATIC_LIBS=ON -DProtobuf_LIBRARIES="-L$GNS_BUILD/../build_protobuf/lib" -DProtobuf_LIBRARIES_PATH="$GNS_BUILD/../build_protobuf/lib" -DProtobuf_INCLUDE_DIR=$GNS_BUILD/../../3rdparty/protobuf/src -DProtobuf_INCLUDE_DIR2=$GNS_BUILD/../build_protobuf/include -DProtobuf_PROTOC_EXECUTABLE=$GNS_BUILD/../build_protoc/protoc $GNS_BUILD/../../3rdparty/GameNetworkingSockets
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     make -j10
+    (($? != 0)) && { printf '%s\n' "Command exited with non-zero"; exit 1; }
     cd ..
     cp build_gns/bin/libGameNetworkingSockets.so .
 fi
@@ -29,10 +38,10 @@ fi
 cmake -DCMAKE_INSTALL_PREFIX=/app .. &&
 make -j10 &&
 cd .. &&
-mkdir -p /app/share/applications/ &&
-mkdir -p /app/bin &&
-cp build_linux64/openjkdf2-64 /app/bin &&
-cp build_linux64/*.so /app/lib &&
-cp build_linux64/libprotobuf.so.3.21.4.0 /app/lib &&
-cp packaging/flatpak/org.openjkdf2.OpenJKDF2.desktop /app/share/applications/ &&
+mkdir -p $FLATPAK_DEST/share/applications/ &&
+mkdir -p $FLATPAK_DEST/bin &&
+cp build_linux64/openjkdf2-64 $FLATPAK_DEST/bin &&
+cp build_linux64/*.so $FLATPAK_DEST/lib &&
+cp build_linux64/libprotobuf.so.3.21.4.0 $FLATPAK_DEST/lib &&
+cp packaging/flatpak/org.openjkdf2.OpenJKDF2.desktop $FLATPAK_DEST/share/applications/ &&
 bash packaging/flatpak/copy_icons.sh packaging/flatpak/icons
