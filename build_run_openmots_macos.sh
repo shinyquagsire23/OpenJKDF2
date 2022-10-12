67c664744e54d0d70b826207d99ff77f01b9ed1f
shinyquagsire23 <mtinc2@gmail.com>
2022-10-11 20:36:48 -0700

    Update my build scripts

--- build_run_openmots_macos.sh
+++ build_run_openmots_macos.sh
@@ -42,11 +42,12 @@ DEBUG_QOL_CHEATS=1 OPENJKDF2_NO_ASAN=0 cmake .. &&
 DEBUG_QOL_CHEATS=1 OPENJKDF2_NO_ASAN=0 make -j10 &&
 cd .. &&
 #cp resource/shaders/* DF2/resource/shaders/ &&
-mkdir -p ~/.local/share/openjkdf2/resource/shaders/ &&
-cp resource/shaders/* ~/.local/share/openjkdf2/resource/shaders/ &&
+mkdir -p "~/Library/Application Support/OpenJKDF2/openjkmots/resource/shaders/" &&
+cp resource/shaders/* "~/Library/Application Support/OpenJKDF2/openjkmots/resource/shaders/" &&
 echo "Running..." &&
 #codesign -s - openjkdf2-64 &&
-#OPENJKDF2_ROOT="/Users/maxamillion/workspace/OpenJKDF2/MOTS" LSAN_OPTIONS="suppressions=/Users/maxamillion/workspace/OpenJKDF2/suppr.txt" ASAN_OPTIONS="log_path=/Users/maxamillion/workspace/OpenJKDF2/asan.log"  lldb -o run ./OpenJKDF2.app/Contents/MacOS/openjkdf2-64 -- -motsCompat #-- -verboseNetworking
+#OPENJKMOTS_ROOT="~/Library/Application Support/OpenJKDF2/openjkmots" LSAN_OPTIONS="suppressions=/Users/maxamillion/workspace/OpenJKDF2/suppr.txt" ASAN_OPTIONS="log_path=/Users/maxamillion/workspace/OpenJKDF2/asan.log"  lldb -o run ./OpenJKDF2.app/Contents/MacOS/openjkdf2-64 -- -motsCompat #-- -verboseNetworking
+#OPENJKDF2_ROOT="~/Library/Application Support/OpenJKDF2/openjkmots" LSAN_OPTIONS="suppressions=/Users/maxamillion/workspace/OpenJKDF2/suppr.txt" ASAN_OPTIONS="log_path=/Users/maxamillion/workspace/OpenJKDF2/asan.log"  lldb -o run ./OpenJKDF2.app/Contents/MacOS/openjkdf2-64 #-- -verboseNetworking
 lldb -o run ./OpenJKDF2.app/Contents/MacOS/openjkdf2-64 -- -motsCompat #-- -verboseNetworking
 #lldb -o run ./OpenJKDF2.app/Contents/MacOS/openjkdf2-64
 #open OpenJKDF2.app
