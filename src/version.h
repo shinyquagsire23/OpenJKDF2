b8d3da793b47e42e6d5c8052fc2e6eeda0aa2276
shinyquagsire23 <mtinc2@gmail.com>
2023-01-15 18:20:45 -0800

    wip scripts

---
+++ src/version.h
@@ -0,0 +1,14 @@
+#ifndef _OPENJKDF2_VERSION_H
+#define _OPENJKDF2_VERSION_H
+
+extern const char* openjkdf2_aReleaseVersion;
+extern const wchar_t* openjkdf2_waReleaseVersion;
+extern const char* openjkdf2_aReleaseCommit;
+extern const wchar_t* openjkdf2_waReleaseCommit;
+extern const char* openjkdf2_aReleaseCommitShort;
+extern const wchar_t* openjkdf2_waReleaseCommitShort;
+extern const int openjkdf2_releaseVersionMajor;
+extern const int openjkdf2_releaseVersionMinor;
+extern const int openjkdf2_releaseVersionPatch;
+
+#endif // _OPENJKDF2_VERSION_H
