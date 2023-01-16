069919b5d36f6d71634ca6de15c09ca734f77854
shinyquagsire23 <mtinc2@gmail.com>
2023-01-15 18:29:00 -0800

    oops

--- distpkg_hotfix.sh
+++ distpkg_hotfix.sh
@@ -4,14 +4,15 @@ OPENJKDF2_ROOT=$(pwd)
 
 python3 scripts/increment_version_commits.py "$1" $(git log -1 --format="%H")
 
-cp packaging/flatpak/org.openjkdf2.OpenJKDF2.yml $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/org.openjkdf2.OpenJKDF2.yml
-
 cd $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/
 git stash
 git checkout master
 git pull
 git checkout "$1"
 git pull
+
+cp $OPENJKDF2_ROOT/packaging/flatpak/org.openjkdf2.OpenJKDF2.yml $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/org.openjkdf2.OpenJKDF2.yml
+
 git add org.openjkdf2.OpenJKDF2.yml
 git commit -m "hotfix"
 git push origin "$1"
--- distpkg_version.sh
+++ distpkg_version.sh
@@ -11,13 +11,14 @@ git push origin master
 
 python3 scripts/increment_version_commits.py "$1" $(git log -1 --format="%H")
 
-cp packaging/flatpak/org.openjkdf2.OpenJKDF2.yml $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/org.openjkdf2.OpenJKDF2.yml
-
 cd $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/
 git stash
 git checkout master
 git pull
 git checkout -b "$1"
+
+cp $OPENJKDF2_ROOT/packaging/flatpak/org.openjkdf2.OpenJKDF2.yml $OPENJKDF2_ROOT/../org.openjkdf2.OpenJKDF2/org.openjkdf2.OpenJKDF2.yml
+
 git add org.openjkdf2.OpenJKDF2.yml
 git commit -m "$1"
 git push origin "$1"
