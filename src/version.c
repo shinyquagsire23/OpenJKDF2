aecdfdab8c9f2ab5d27b4033fa800e97f9164c18
shinyquagsire23 <mtinc2@gmail.com>
2023-01-25 00:28:24 -0800

    v0.8.4

--- src/version.c
+++ src/version.c
@@ -1,12 +1,12 @@
 #include "types.h"
 #include "version.h"
 
-const char* openjkdf2_aReleaseVersion = "v0.8.3";
-const wchar_t* openjkdf2_waReleaseVersion = L"v0.8.3";
+const char* openjkdf2_aReleaseVersion = "v0.8.4";
+const wchar_t* openjkdf2_waReleaseVersion = L"v0.8.4";
 const char* openjkdf2_aReleaseCommit = OPENJKDF2_RELEASE_COMMIT;
 const wchar_t* openjkdf2_waReleaseCommit = OPENJKDF2_RELEASE_COMMIT_W;
 const char* openjkdf2_aReleaseCommitShort = OPENJKDF2_RELEASE_COMMIT_SHORT;
 const wchar_t* openjkdf2_waReleaseCommitShort = OPENJKDF2_RELEASE_COMMIT_SHORT_W;
 const int openjkdf2_releaseVersionMajor = 0;
 const int openjkdf2_releaseVersionMinor = 8;
-const int openjkdf2_releaseVersionPatch = 3;
+const int openjkdf2_releaseVersionPatch = 4;
