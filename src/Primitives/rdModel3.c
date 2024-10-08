972407f5f88d94f5a7fecb12b513765d2ef884c6
LadyEebs <ladyeebs@gmail.com>
2024-10-08 13:21:15 -0400

    Fixed culling and alpha test for 3dos

--- src/Primitives/rdModel3.c
+++ src/Primitives/rdModel3.c
@@ -1638,11 +1638,18 @@ int rdModel3_DrawFace(rdFace *face, int lightFlags)
 	{
 		alpha = 90.0f / 255.0f;
 		rdSetBlendMode(RD_BLEND_MODE_ALPHA);
+		rdSetZBufferMethod(RD_ZBUFFER_READ_NOWRITE);
 	}
 	else
 	{
 		rdSetBlendMode(RD_BLEND_MODE_NONE);
+		rdSetZBufferMethod(RD_ZBUFFER_READ_WRITE);
 	}
+
+	if ((face->type & RD_FF_DOUBLE_SIDED) != 0)
+		rdSetCullMode(RD_CULL_MODE_NONE);
+	else
+		rdSetCullMode(RD_CULL_MODE_CCW_ONLY);
 
 	if (rdBeginPrimitive(RD_PRIMITIVE_TRIANGLE_FAN))
 	{
